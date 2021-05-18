756f832544ef4516045e0ab284cfdf320460437c
dlanoronald123 <dlanoronald123@gmail.com>
2021-05-18 21:18:35 +0800

    Minor Revisions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/articles', to: 'articles#index'
   get '/articles/new', to: 'articles#new', as: 'new_article'
-  get '/articles/:id', to: 'articles#show', as: 'show_article'
   post '/articles', to: 'articles#create', as: 'create_article' 
+  get '/articles/:id', to: 'articles#show', as: 'show_article'
   get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
   patch '/articles/:id', to: 'articles#update', as: 'update_article'
   delete '/articles/:id', to: 'articles#destroy', as: 'delete_article'
